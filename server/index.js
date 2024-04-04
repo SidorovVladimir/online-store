13807603607617e7fe0a6b9136f6f774d76c7f3a
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2024-04-04 22:24:48 +0700

    adds model db

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 import sequelize from './db.js';
 
+import { User, Basket, BasketDevice, Device, Rating, DeviceInfo, Type, Brand, TypeBrand } from './models/models.js';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -17,6 +19,7 @@ const start = async () => {
     });
 
   } catch(e) {
+    console.log('Errors started DateBase');
     console.log(e);
   }
 };
