2394302b3508b646517a3a0a977308cb04c58669
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2024-05-02 21:44:39 +0700

    adds components Createtype, CreateBrand, CreateDevice and updates pages Admin

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import { Context } from "../index.js";
 import { Navbar, Container, Nav, Button } from "react-bootstrap"
 import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utils/consts.js";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts.js";
 import { observer } from 'mobx-react-lite';
+import { useNavigate } from "react-router-dom";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const navigate = useNavigate();
   return (
     <Navbar bg="dark" data-bs-theme="dark">
     <Container>
@@ -14,8 +16,8 @@ const NavBar = observer(() => {
 
       { user.isAuth ?
         <Nav className="ml-auto" style={{color: 'white'}}>
-          <Button variant="outline-light">Админ панель</Button>
-          <Button variant="outline-light" className="ms-2">Выйти</Button>
+          <Button variant="outline-light" onClick={() => navigate(ADMIN_ROUTE)}>Админ панель</Button>
+          <Button variant="outline-light" className="ms-2" onClick={() => navigate(LOGIN_ROUTE)}>Выйти</Button>
         </Nav>
         :
         <Nav className="ml-auto" style={{color: 'white'}}>
